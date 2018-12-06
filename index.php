6d4928fa108557d32ea1c5f481e54e700e4ed22f
Bosko Boskovic <bosko.boskovic@gmail.com>
2018-12-06 12:42:19 +0100

    =Project folder structure

--- index.php
+++ index.php
@@ -1,27 +1,47 @@
 <!DOCTYPE html>
-<html>
-<head>
+<html lang="en">
+
+  <head>
+
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Quantox backend task</title>
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Php backend test - Quantox company</title>
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/twitter/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
 
+    <style>
+    	body {
+          padding-top: 3.5rem;
+         }
+    </style>
 
+  </head>
 
+  <body>
 
+	<?php include_once('pages/template/nav.php'); ?>
 
+    <main role="main">
 
+      <!-- Main jumbotron for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <div class="container">
+          <h1 class="display-3">Hello, guys!</h1>
+          <p>Welcome to php backend test. Here you can search registered users. You are just one step from there.</p>
+          <p><a class="btn btn-success btn-sm" href="#" role="button">Login with you account &raquo;</a> <a class="btn btn-primary btn-sm" href="#" role="button">Create new account &raquo;</a></p>
+          
+        </div>
+      </div>
 
+    </main>
 
+    <footer class="container">
+      <p>&copy; Quantox <?php echo date('Y'); ?></p>
+    </footer>
 
+    <!-- Bootstrap core JavaScript -->    
+    <script src="vendor/twitter/bootstrap/dist/css/bootstrap.min.js"></script>
 
+  </body>
 
-    
-
-</body>
-
-
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.js" rel="stylesheet">
 </html>
