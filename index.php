f1c55b06676c9061e0b3a0f83b23f6c07bf7c8b3
Bosko Boskovic <bosko.boskovic@gmail.com>
2018-12-06 12:15:55 +0100

    =Init commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Quantox backend task</title>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+
+
+
+
+
+
+
+
+
+
+    
+
+</body>
+
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.js" rel="stylesheet">
+</html>
